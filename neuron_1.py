007e67732c3e1c5a601b23a459575c3491792e2b
tumuruub0108 <Tumuruub0108@gmail.com>
2025-09-25 17:36:07 +0900

    I used the model to make predictions

--- neuron_1.py
+++ neuron_1.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 from keras.datasets import fashion_mnist
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
-
+# First 55,000 → training set.
 X_train, y_train = X_train_full[:-5000], y_train_full[:-5000]
+# Last 5,000 → validation se
 X_valid, y_valid = X_train_full[-5000:], y_train_full[-5000:]
 
 X_train, X_valid, X_test = X_train / 255., X_valid / 255., X_test / 255.
@@ -64,4 +66,28 @@ plt.show()
 """
 
 # print(model.evaluate(X_test, y_test))
-# 504pages
+
+"""
+sequence[start:stop:step]
+    start → index where slicing begins (default = 0).
+    stop → index where slicing stops (but not included).
+    step → how many elements to skip each time (default = 1).
+    
+nums = [10, 20, 30, 40, 50]
+
+print(nums[1:4])    # [20, 30, 40] → elements at indices 1,2,3
+print(nums[:])      # [10, 20, 30, 40, 50] → copy the whole list
+print(nums[::2])    # [10, 30, 50] → every 2nd element
+print(nums[-3:])    # [30, 40, 50] → last 3 elements
+print(nums[::-1])   # [50, 40, 30, 20, 10] → reverse the list
+
+"""
+
+# Using the model to make predictions
+X_new = X_test[:3]
+y_proba = model.predict(X_new)
+# print(y_proba.round(2))
+
+y_pred = y_proba.argmax(axis=-1)
+# sprint(y_pred)
+# print(np.array(class_names)[y_pred]) 506
