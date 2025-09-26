dd83faa8ea52a6dfd4d84cf32cb77920dda9e44b
tumuruub0108 <Tumuruub0108@gmail.com>
2025-09-26 17:56:14 +0900

    added some things

--- neuron_1.py
+++ neuron_1.py
@@ -89,5 +89,75 @@ y_proba = model.predict(X_new)
 # print(y_proba.round(2))
 
 y_pred = y_proba.argmax(axis=-1)
-# sprint(y_pred)
+# print(y_pred)
 # print(np.array(class_names)[y_pred]) 506
+
+y_new = y_test[:3]
+# print(y_new)
+
+# Building a Regression MLP Using the Sequential API
+tf.random.set_seed(42)
+
+"""
+
+norm_layer = tf.keras.layers.Normalization(input_shape=X_train.shape[1:])
+model = tf.keras.Sequential([
+    norm_layer,
+    tf.keras.layers.Dense(50, activation="relu"),
+    tf.keras.layers.Dense(50, activation="relu"),
+    tf.keras.layers.Dense(50, activation="relu"),
+    tf.keras.layers.Dense(1)
+])
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+model.compile(loss="mse", optimizer=optimizer, metrics=["RootMeanSquaredError"])
+norm_layer.adapt(X_train)
+
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+mse_test, rmse_test = model.evaluate(X_test, y_test)
+X_new = X_test[:3]
+y_pred = model.predict(X_new)
+print(y_pred)
+
+"""
+
+
+# Building Complex Models Using the Functional API
+"""
+
+# Wide & Deep neural network
+normalization_layer = tf.keras.layers.Normalization()
+hidden_layer1 = tf.keras.layers.Dense(30, activation="relu")
+hidden_layer2 = tf.keras.layers.Dense(30, activation="relu")
+concat_layer = tf.keras.layers.Concatenate()
+output_layer = tf.keras.layers.Dense(1)
+
+input_ = tf.keras.layers.Input(shape=X_train.shape[1:])
+normalized = normalization_layer(input_)
+hidden1 = hidden_layer1(normalized)
+hidden2 = hidden_layer2(hidden1)
+concat = concat_layer([normalized, hidden2])
+output = output_layer(concat)
+
+model = tf.keras.Model(inputs=[input_], outputs=[output])
+
+"""
+
+# handling multiple inputs
+input_wide = tf.keras.layers.Input(shape=[5]) # features 0 to 4
+input_deep = tf.keras.layers.Input(shape=[6]) # features 2 to 7
+
+norm_layer_wide = tf.keras.layers.Normalization()
+norm_layer_deep = tf.keras.layers.Normalization()
+
+norm_wide = norm_layer_wide(input_wide)
+norm_deep = norm_layer_deep(input_deep)
+
+hidden1 = tf.keras.layers.Dense(30, activation="relu")(norm_deep)
+hidden2 = tf.keras.layers.Dense(30, activation="relu")(hidden1)
+
+concat = tf.keras.layers.concatenate([norm_wide, hidden2])
+output = tf.keras.layers.Dense(1)(concat)
+
+model = tf.keras.Model(inputs=[input_wide, input_deep], outputs=[output])
+
+print(model) # 511
